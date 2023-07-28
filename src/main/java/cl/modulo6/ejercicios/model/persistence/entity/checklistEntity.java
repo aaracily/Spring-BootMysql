d08598a464761ddef0c4c816128d5ea054bf7781
Simon Zelada <128656840+supersimonkybone@users.noreply.github.com>
2023-07-28 18:16:27 -0400

    Add files via upload

---
+++ src/main/java/cl/modulo6/ejercicios/model/persistence/entity/checklistEntity.java
@@ -0,0 +1,24 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "checklist")
+public class Checklist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_capacitacion")
+    private Long idCapacitacion;
+
+    private boolean charla;
+    private boolean asistencia;
+    private boolean pago;
+    private boolean cierre;
+
+    @Column(name = "id_profesional")
+    private Long idProfesional;
+
+    @Column(name = "fecha_ultima_visita")
+    private Date fechaUltimaVisita;
+
+}
