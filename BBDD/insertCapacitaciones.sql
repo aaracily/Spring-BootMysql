b6a37da5e4b7a00bf7df25e5758a2886e028ebff
MauricioJMoralesV <129233232+MauricioJMoralesV@users.noreply.github.com>
2023-07-27 20:47:17 -0400

    comit 2

--- BBDD/insertCapacitaciones.sql
+++
@@ -1,8 +0,0 @@
-select * from capacitacion;
-insert into capacitacion ( nombre, detalle, fecha,hora,lugar,duracion,cantidad)
-values("primero auxilios","otorga conocimientos necesarios para endrentar una emergencia", "2023-10-23",1400,"Hall center",20,30); 
-insert into capacitacion ( nombre, detalle, fecha,hora,lugar,duracion,cantidad)
-values("Control de peligros y riesgos en oficinas","otorga conocimientos necesarios para enfrentar una emergencia", "2023-09-23",1400,"Hall center",10,30),
-("Prevención de incendios en lugar de trabajo","otorga conocimientos necesarios para enfrentar una emergencia", "2023-06-13",1000,"Hall center",15,20),
-("Uso y manejo de extintores portátiles ","otorga conocimientos necesarios para enfrentar una emergencia", "2023-12-13",1000,"Hall center",15,20),
-("Formación de brigadas de emergencia ","otorga conocimientos necesarios para enfrentar una emergencia", "2023-09-10",1000,"Hall center",20,30); 
--- BBDD/insertUsuario.sql
+++
@@ -1,5 +0,0 @@
-select * from usuario;
-insert into usuario (nombre, username, password) 
-values ("cliente1","cliente1","primeraclave"),
-("cliente2","cliente2","segundaclave"),
-("cliente3","cliente3","terceraclave");
